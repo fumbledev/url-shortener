5fa9b6bcff6dc7b44ec4931b80febf7c859b7a6b
Udanth <udanthhreddy@gmail.com>
2025-05-31 18:35:06 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import { Children } from 'react'
+import AppLayout from './layouts/AppLayout.jsx'
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import Auth from './pages/Auth'
+import Link from './pages/Link'
+import RedirectLink from './pages/RedirectLink'
+
+const router = createBrowserRouter([
+  {
+    element : <AppLayout/>,
+    children : [
+      {
+        path : "/",
+        element : <LandingPage/>
+      },
+      {
+        path : "/dashboard",
+        element : <Dashboard/>
+      },
+      {
+        path : "/auth",
+        element : <Auth/>
+      },
+      {
+        path : "/link/:id",
+        element : <Link/>
+      },
+      {
+        path : "/:id",
+        element : <RedirectLink/>
+      }
+    ]
+  }
+])
+
+const App = () => {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default App
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router-dom'
+
+const LandingPage = () => {
+
+  const [longUrl,setLongUrl] = useState()
+  const navigate = useNavigate()
+
+  const handleShorten = (event) =>{
+    event.preventDefault()
+    if(longUrl){
+      navigate(`/auth?createNew=${longUrl}`)
+    }
+  }
+  return (
+    <div className='flex flex-col items-center'>
+      <h2 className='my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold'>
+        The only URL Shortener <br /> you need!👇
+      </h2>
+      <form
+       className='sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2'
+       onSubmit={handleShorten}
+      >
+        <Input 
+          type="url" 
+          value = {longUrl}
+          placeholder="Enter your long URL"
+          onChange={(event)=>setLongUrl(event.target.value)}
+          className="h-full flex-1 py-4 px-4"
+        />
+        <Button
+         className="h-full" 
+         type="submit"
+          variant="destructive"
+        >
+          Shorten!
+        </Button>
+      </form>
+      <img src="/banner.jpeg" alt="banner" className='w-full my-11 md:px-11'/>
+      <Accordion type="multiple" collapsible className="w-full md:px-11">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              How does this URL Shortener works?
+            </AccordionTrigger>
+            <AccordionContent>
+              When you enter a long URl, our system generates a shorter version of
+              that URL, This shortend URL redirects to the original long URL when 
+              accessed.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+              Do I need an account touse the app?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. Creating an account allows you to manage your URLs, view 
+              analytics, and customize your short URLs.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+              What analytics are available for my shortened URLs?
+            </AccordionTrigger>
+            <AccordionContent>
+              You can view the number of clicks, geolocation data of the clicks
+              and device types (mobile/desktop) for each of your shortend URLs.
+            </AccordionContent>
+          </AccordionItem>
+      </Accordion>
+    </div>
+  )
+}
+
+export default LandingPage
